59c6d669ae1f4bb6eb642a76dc8fc49e9b912eeb
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-11-04 22:25:43 +0300

    MembershipContext eklendi

---
+++ MemberRegistration.DataAccess/Concrete/EntityFramework/MembershipContext.cs
@@ -0,0 +1,20 @@
+﻿using MemberRegistration.Entities.Concrete;
+using System.Data.Entity;
+
+namespace MemberRegistration.DataAccess.Concrete.EntityFramework
+{
+    public class MembershipContext : DbContext
+    {
+        public MembershipContext()
+        {
+            Database.SetInitializer<MembershipContext>(null);
+        }
+
+        public DbSet<Member> Members { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new MemberMap());
+        }
+    }
+}
