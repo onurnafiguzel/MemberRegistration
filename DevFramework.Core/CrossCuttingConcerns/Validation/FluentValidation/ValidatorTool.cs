71df65b4762a8f9cc3a57d127b2e77147e72054e
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-11-04 21:53:35 +0300

    DevFramework Projesinden Core katmanı eklendi.

---
+++ DevFramework.Core/CrossCuttingConcerns/Validation/FluentValidation/ValidatorTool.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Core.CrossCuttingConcerns.Validation.FluentValidation
+{
+    public class ValidatorTool
+    {
+        public static void FluentValidate(IValidator validator, object entity)
+        {
+            var context = new ValidationContext<object>(entity);
+            //ProductValidator productValidator = new ProductValidator(); Gerek yok artık
+            var result = validator.Validate(context);
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
+
+            //var result = validator.Validate((IValidationContext)entity); //Sadece entity kabul etmiyor
+
+            //if (result.Errors.Count > 0)
+            //{
+            //    throw new ValidationException(result.Errors);
+            //}
+        }
+    }
+}
