869bc243e96077d4ab3034d68c9dfb447c6f360e
onurnafiguzel <58780366+onurnafiguzel@users.noreply.github.com>
2021-11-04 22:13:08 +0300

    Member sınıfı eklendi

---
+++ MemberRegistration.Entities/Concrete/Member.cs
@@ -0,0 +1,15 @@
+﻿using DevFramework.Core.Entities;
+using System;
+
+namespace MemberRegistration.Entities.Concrete
+{
+    public class Member : IEntity
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string TcNo { get; set; }
+        public string Email { get; set; }
+    }
+}
